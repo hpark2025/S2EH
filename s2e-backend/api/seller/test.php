ed41669fe1ad9e3413e6c074b6d79682488c6068
unknown <hplaza292@gmail.com>
2025-11-03 15:59:30 +0800

    Remove test files from repository

--- s2e-backend/api/seller/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode([
-    'success' => true,
-    'message' => 'Seller endpoint test successful!',
-    'method' => $_SERVER['REQUEST_METHOD'],
-    'uri' => $_SERVER['REQUEST_URI']
-]);
-
